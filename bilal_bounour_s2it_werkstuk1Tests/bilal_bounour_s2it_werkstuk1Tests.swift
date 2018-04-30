c59e29f57a3af40c512a13de5b67302a7fc138b5
ontlener <ontlener@ontleners-macbook-pro.local>
2018-04-30 15:28:33 +0200

    Initial Commit

---
+++ bilal_bounour_s2it_werkstuk1Tests/bilal_bounour_s2it_werkstuk1Tests.swift
@@ -0,0 +1,36 @@
+//
+//  bilal_bounour_s2it_werkstuk1Tests.swift
+//  bilal_bounour_s2it_werkstuk1Tests
+//
+//  Created by ontlener on 30/04/2018.
+//  Copyright © 2018 bilal_bounour. All rights reserved.
+//
+
+import XCTest
+@testable import bilal_bounour_s2it_werkstuk1
+
+class bilal_bounour_s2it_werkstuk1Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
